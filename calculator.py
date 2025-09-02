7edca9c1250583205d7ccf7d263b01b0364579c0
wonderalone7 <68640314+wonderalone7@users.noreply.github.com>
2025-09-02 20:05:26 +0530

    fixed the bug

--- calculator.py
+++ calculator.py
@@ -1,22 +1,27 @@
 # Broken Calculator Program (for Git practice)
 
 def add(a, b):
-    return a - b   # ❌ Wrong operator
+    return a + b   # ❌ Wrong operator
 
 def subtract(a, b):
-    return a + b   # ❌ Wrong operator
+    return a - b   # ❌ Wrong operator
 
 def multiply(a, b):
     return a * b
 
 def divide(a, b):
     # ❌ Doesn't handle division by zero
-    return a / b
+    if (b==0):
+        print("divide by zero exception");
+        return none
+    else:
+        return a / b
+    
 
 if __name__ == "__main__":
     print("Simple Calculator")
-    x = input("Enter first number: ")   # ❌ input returns string, not int
-    y = input("Enter second number: ")
+    x = int(input("Enter first number: "))   # ❌ input returns string, not int
+    y = int(input("Enter second number: "))
 
     print("Addition:", add(x, y))
     print("Subtraction:", subtract(x, y))
